874212db562515e54a4731e91772ccb0a1316c35
necrozombie88 <51805627+necrozombie88@users.noreply.github.com>
2020-07-20 22:10:18 -0700

    work on filter region started

--- public/script.js
+++ public/script.js
@@ -8,9 +8,11 @@ async function test() {
 
     const data = await response.json();
     return data;
+    
 }
 
-fetch("https://restcountries.eu/rest/v2/all")
+function addToDOM() {  
+    fetch("https://restcountries.eu/rest/v2/all")
     .then((data) => data.json())
     .then((data) => {
         data.forEach( (data) => {
@@ -27,3 +29,25 @@ fetch("https://restcountries.eu/rest/v2/all")
 
         countryContainer.innerHTML = cards.join(" ");
     });
+}
+
+addToDOM()
+
+    // this works to remove the unwanted cards by region but needs to reload all countries back into the DOM
+    //might need to make addToDOM async so that it may be called every time it changes
+    // adding addToDOM to this event as it is does not work to refresh the DOM
+selectRegion.addEventListener('change', () => {
+
+    document.querySelectorAll('.card').forEach( card => {
+        if (!card.innerHTML.includes(`Region: ${selectRegion.value}`)  ) {
+            countryContainer.removeChild(card);
+          }
+
+})
+
+
+window.addEventListener('click', (e) => {
+    if (e.target.innerHTML.includes("Region: Asia")) {
+        // console.log(e.target.innerHTML)
+    }
+})
