577793a36bcfeb8b7617b548330bfbd3eebbd49a
Adam <adamjwatt88@gmail.com>
2020-07-22 15:35:44 -0700

    made changes to render index.ejs that works with express

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
 const express = require('express');
 const app = express();
+const request = require('request');
+
+
+async function addToDOM() {
+    const response = await fetch("https://restcountries.eu/rest/v2/all")
+    const data = await response.json()
+    data.forEach((data) => {
+                cards.push(`
+  <div class="card">
+    <img src="${data.flag}" alt="${data.name}"/>
+    <h3>${data.name}</h3>
+    <span>Population: ${data.population}</span>
+    <span>Region: ${data.region}</span>
+    <span>Capital: ${data.capital}</span>
+  </div> 
+  `);
+            });
+
+            countryContainer.innerHTML = cards.join(" ");
+}
+
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 
-
-
-
-app.get("/", function (req, res) {
-    res.render("index.ejs")
+app.get("/", (req, res) => {
+    let url = "https://restcountries.eu/rest/v2/all"
+    request(url, (error,response, body) => {
+        if(!error && response.statusCode == 200) {
+            let data = JSON.parse(body)
+            res.render('index.ejs', { data: data} )
+        } 
+    } )
 });
+
+app.get("/region-selector", (req, res) => {
+    let query = req.body.selectRegion;
+    let url = `https://restcountries.eu/rest/v2/region/${query}`
+    console.log(query)
+    request(url, (error,response, body) => {
+        if(!error && response.statusCode == 200) {
+            let data = JSON.parse(body)
+            res.render('index.ejs', { data: data} )
+        } 
+    } )
+
+})
 
 app.post("/region-selector", function (req, res) {
         // let region = req.body.region
--- public/script.js
+++ public/script.js
@@ -1,61 +1,17 @@
 const countryContainer = document.getElementById("country-container");
 const selectRegion = document.getElementById("select-region");
 
-let cards = [];
-
 async function test() {
     const response = await fetch("https://restcountries.eu/rest/v2/all");
 
     const data = await response.json();
     return data;
-
 }
-
-// function addToDOM() {
-//     fetch("https://restcountries.eu/rest/v2/all")
-//         .then((data) => data.json())
-//         .then((data) => {
-//             data.forEach((data) => {
-//                 cards.push(`
-//   <div class="card">
-//     <img src="${data.flag}" alt="${data.name}"/>
-//     <h3>${data.name}</h3>
-//     <span>Population: ${data.population}</span>
-//     <span>Region: ${data.region}</span>
-//     <span>Capital: ${data.capital}</span>
-//   </div> 
-//   `);
-//             });
-
-//             countryContainer.innerHTML = cards.join(" ");
-//         });
-// }
-
-async function addToDOM() {
-    const response = await fetch("https://restcountries.eu/rest/v2/all")
-    const data = await response.json()
-    data.forEach((data) => {
-                cards.push(`
-  <div class="card">
-    <img src="${data.flag}" alt="${data.name}"/>
-    <h3>${data.name}</h3>
-    <span>Population: ${data.population}</span>
-    <span>Region: ${data.region}</span>
-    <span>Capital: ${data.capital}</span>
-  </div> 
-  `);
-            });
-
-            countryContainer.innerHTML = cards.join(" ");
-        
-}
-
-
-addToDOM()
 
 // this works to remove the unwanted cards by region but needs to reload all countries back into the DOM
 //might need to make addToDOM async so that it may be called every time it changes
 // adding addToDOM to this event as it is does not work to refresh the DOM
+
 // selectRegion.addEventListener('change', () => {
     
 //     document.querySelectorAll('.card').forEach(card => {
@@ -67,8 +23,8 @@ addToDOM()
 // })
 
 
-window.addEventListener('click', (e) => {
-    if (e.target.innerHTML.includes("Region: Asia")) {
-        // console.log(e.target.innerHTML)
-    }
-})
+// window.addEventListener('click', (e) => {
+//     if (e.target.innerHTML.includes("Region: Asia")) {
+//         console.log(e.target.innerHTML)
+//     }
+// })
