fc72ebc0cb49f2ec1afc23b848b47e208d7c10a8
necrozombie88 <51805627+necrozombie88@users.noreply.github.com>
2020-07-24 23:28:24 -0700

    need to finish styles for region.ejs

--- app.js
+++ app.js
@@ -2,25 +2,6 @@ const express = require('express');
 const app = express();
 const request = require('request');
 
-
-async function addToDOM() {
-    const response = await fetch("https://restcountries.eu/rest/v2/all")
-    const data = await response.json()
-    data.forEach((data) => {
-                cards.push(`
-  <div class="card">
-    <img src="${data.flag}" alt="${data.name}"/>
-    <h3>${data.name}</h3>
-    <span>Population: ${data.population}</span>
-    <span>Region: ${data.region}</span>
-    <span>Capital: ${data.capital}</span>
-  </div> 
-  `);
-            });
-
-            countryContainer.innerHTML = cards.join(" ");
-}
-
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 
@@ -39,7 +20,6 @@ app.get("/regionSelector", (req, res) => {
     let query = req.query.selectRegion;
     let url = `https://restcountries.eu/rest/v2/region/${query}`
 
-    console.log(query)
     request(url, (error,response, body) => {
         if(!error && response.statusCode == 200) {
             let data = JSON.parse(body)
@@ -49,26 +29,16 @@ app.get("/regionSelector", (req, res) => {
 })
 
 
-// loop through all the names of the countries and store them into an array then attach the value to the end of req.query
 app.get("/region", (req,res) => {
-    let query = req.query.Antigua
-    let url = `https://restcountries.eu/rest/v2/name/${query}?fullText=true`;
-    console.log(query)
+    let query = req.query.alphaCodes
+    let url = `https://restcountries.eu/rest/v2/alpha/${query}`;
     request(url, (error,response, body) => {
         if(!error && response.statusCode == 200) {
             let data = JSON.parse(body)
-            res.render('region.ejs', { countries: data} )
+            res.render('region.ejs', { country: data} )
         } 
     } )
 })
-
-
-app.post("/region-selector", function (req, res) {
-        // let region = req.body.region
-
-        res.render('region.ejs')
-    }
-)
 
 
 app.listen(3000, () => {
--- public/script.js
+++ public/script.js
@@ -9,21 +9,6 @@ async function test() {
     return data;
 }
 
-// this works to remove the unwanted cards by region but needs to reload all countries back into the DOM
-//might need to make addToDOM async so that it may be called every time it changes
-// adding addToDOM to this event as it is does not work to refresh the DOM
-
-// selectRegion.addEventListener('change', () => {
-    
-//     document.querySelectorAll('.card').forEach(card => {
-//         if (!card.innerHTML.includes(`Region: ${selectRegion.value}`)) {
-//             countryContainer.removeChild(card);
-//         }
-
-//     })
-// })
-
-
 // window.addEventListener('click', (e) => {
 //     if (e.target.innerHTML.includes("Region: Asia")) {
 //         console.log(e.target.innerHTML)
